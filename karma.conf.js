1d7c2414a4d0fbdf814d1b04d17391594dcb136a
Matt Follett <matt.follett@gmail.com>
2014-05-26 21:18:20 -0500

    Add some scaffolding.

---
+++ karma.conf.js
@@ -0,0 +1,28 @@
+// Karma configuration
+
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    files: [
+      'bower_components/angular/angular.js',
+      'bower_components/angular-mocks/angular-mocks.js',
+      'toc.js',
+      'specs/*.js'
+    ],
+
+    reporters: ['progress'],
+
+    port: 9876,
+    colors: true,
+
+    logLevel: config.LOG_INFO,
+
+    browsers: ['Chrome'],
+    frameworks: ['jasmine'],
+
+    captureTimeout: 60000,
+
+    autoWatch: true,
+    singleRun: false
+  });
+};
